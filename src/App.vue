3a3af78a149d263c9865437e31a2bb6a128a8e5c
Hanks <zhanghan.me@gmail.com>
2016-11-22 18:19:46 +0800

    create basic files

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="wrapper">
+    <image class="logo" src="https://vuejs.org/images/logo.png"></image>
+    <div class="intro">
+      <text class="title">Weex Vue Demo</text>
+      <text class="subtitle">This component is written in Vue 2.0 Syntax.</text>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  .logo {
+    margin-left: 250;
+    margin-bottom: 30;
+    width: 250;
+    height: 250;
+  }
+  .title {
+    font-size: 60;
+    color: #505050;
+    text-align: center;
+  }
+  .subtitle {
+    font-size: 30;
+    color: #AAAAAA;
+    text-align: center;
+    margin-top: 20;
+  }
+</style>
---
+++ src/entry.vue
@@ -0,0 +1,17 @@
+<template>
+  <app></app>
+</template>
+
+<script>
+  module.exports = {
+    components: {
+      'app': require('./App.vue')
+    },
+    created: function () {
+      console.log('Entry created.')
+    },
+    mounted: function () {
+      console.log('Entry mounted.')
+    }
+  }
+</script>
