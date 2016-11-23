beade42b298fcedd6d10217bf191b5b0a39fb9e6
Hanks <zhanghan.me@gmail.com>
2016-11-23 22:33:24 +0800

    add webpack build script

---
+++ build/webpack.weex.config.js
@@ -0,0 +1,30 @@
+var path = require('path')
+var webpack = require('webpack')
+
+var bannerPlugin = new webpack.BannerPlugin(
+  '// { "framework": "Vue" }\n',
+  { raw: true }
+)
+
+module.exports = {
+  entry: {
+    'vue-bundle': path.resolve('src', 'entry.vue?entry=true')
+  },
+  output: {
+    path: 'dist',
+    filename: '[name].weex.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader'],
+        exclude: /node_modules/
+      }, {
+        test: /\.vue(\?[^?]+)?$/,
+        loaders: ['weex-vue-loader']
+      }
+    ]
+  },
+  plugins: [bannerPlugin]
+}
