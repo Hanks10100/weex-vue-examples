65174ee8f6183c698f6841675f5371b9242c7f19
Hanks <zhanghan.me@gmail.com>
2016-11-24 11:27:27 +0800

    simplify the demo

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="wrapper">
-    <image class="logo" src="https://vuejs.org/images/logo.png"></image>
-    <div class="intro">
-      <text class="title">Weex Vue Demo</text>
-      <text class="subtitle">This component is written in Vue 2.0 Syntax.</text>
-    </div>
+    <text class="title">Weex Vue Demo</text>
+    <text class="subtitle">This component is written in Vue 2.0 Syntax.</text>
   </div>
 </template>
 
@@ -14,12 +11,6 @@
     flex-direction: column;
     justify-content: center;
     height: 100%;
-  }
-  .logo {
-    margin-left: 250px;
-    margin-bottom: 30px;
-    width: 250px;
-    height: 250px;
   }
   .title {
     font-size: 60px;
