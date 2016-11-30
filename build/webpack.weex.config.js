33b244ef8a5fe4bed569666b653a2f891aad4027
Hanks <zhanghan.me@gmail.com>
2016-11-30 14:59:59 +0800

    add image demo

--- build/webpack.weex.config.js
+++ build/webpack.weex.config.js
@@ -9,6 +9,7 @@ var bannerPlugin = new webpack.BannerPlugin(
 module.exports = {
   entry: {
     'examples/hello': path.resolve('src/examples', 'hello.vue?entry=true'),
+    'examples/image': path.resolve('src/examples', 'image.vue?entry=true'),
     'vue-bundle': path.resolve('src', 'entry.vue?entry=true')
   },
   output: {
---
+++ src/examples/image.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="wrapper">
+    <image src="https://alibaba.github.io/weex/img/weex_logo_blue@3x.png" class="logo"></image>
+    <text class="title">Weex</text>
+  </div>
+</template>
+
+<style>
+  .wrapper {align-items: center; margin-top: 120px;}
+  .title {font-size: 48px;}
+  .logo {width: 360px; height: 82px;}
+</style>
