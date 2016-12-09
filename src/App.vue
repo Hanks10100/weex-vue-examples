10cebe7dfa8065c7b22663c1ab9d5eb3f179e7a0
Hanks <zhanghan.me@gmail.com>
2016-12-09 16:38:27 +0800

    add switch demo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="wrapper">
-    <text class="title">Weex Vue Demo</text>
-    <text class="subtitle">This component is written in Vue 2.0 Syntax.</text>
+    <switch-demo></switch-demo>
   </div>
 </template>
+
+<script>
+  export default {
+    components: {
+      'switch-demo': require('./components/switch-demo.vue')
+    }
+  }
+</script>
 
 <style scoped>
   .wrapper {
@@ -11,16 +18,5 @@
     flex-direction: column;
     justify-content: center;
     height: 100%;
-  }
-  .title {
-    font-size: 60px;
-    color: #505050;
-    text-align: center;
-  }
-  .subtitle {
-    font-size: 30px;
-    color: #AAAAAA;
-    text-align: center;
-    margin-top: 20px;
   }
 </style>
