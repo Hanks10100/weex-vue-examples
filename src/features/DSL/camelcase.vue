0daa83be920894ac7f03051015a178a2c201bea1
Hanks <zhanghan.me@gmail.com>
2017-02-16 15:13:26 +0800

    add camelcase test case

---
+++ src/features/DSL/camelcase.vue
@@ -0,0 +1,15 @@
+<template>
+  <list class="group">
+    <cell v-for="name in lists">
+      <text :data-name="name" class="text">{{name}}</text>
+    </cell>
+  </list>
+</template>
+
+<script>
+  module.exports = {
+    data: function () {
+      return { lists: ['A', 'B'] }
+    }
+  }
+</script>
