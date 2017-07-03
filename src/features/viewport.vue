2e61695a824835f14c446286ade03053188a60ba
Hanks <zhanghan.me@gmail.com>
2017-07-03 14:33:45 +0800

    add viewport example

---
+++ src/features/viewport.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="box750">
+      <text class="text">750</text>
+      <div class="box640">
+        <text class="text">640</text>
+        <div class="box480">
+          <text class="text">480</text>
+          <div class="box360">
+            <text class="text">360</text>
+            <div class="box240">
+              <text class="text">240</text>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .box750 {
+    width: 750px;
+    height: 750px;
+    background-color: #E0E0E0;
+  }
+  .box640 {
+    width: 640px;
+    height: 640px;
+    background-color: #CCCCCC;
+  }
+  .box480 {
+    width: 480px;
+    height: 480px;
+    background-color: #A8A8A8;
+  }
+  .box360 {
+    width: 360px;
+    height: 360px;
+    background-color: #909090;
+  }
+  .box240 {
+    width: 240px;
+    height: 240px;
+    background-color: #777777;
+  }
+  .text {
+    font-size: 46px;
+  }
+</style>
