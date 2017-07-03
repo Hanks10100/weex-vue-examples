e21828cb29aa754dce64cda86a6ef8d5feef2efa
Hanks <zhanghan.me@gmail.com>
2017-07-03 14:20:11 +0800

    add example of import css file

---
+++ src/features/DSL/import-css.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wrapper">
+    <text class="weex">Hello Weex !</text>
+    <text class="vue">Hello Vue !</text>
+  </div>
+</template>
+
+<!-- <style src="./shared.css" scoped></style> -->
+
+<style scoped>
+  @import "./shared.css";
+
+  .wrapper {
+    flex-direction: column;
+    justify-content: center;
+  }
+</style>
