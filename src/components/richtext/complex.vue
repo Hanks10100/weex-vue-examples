5478a1966db0e0961858b2fdf8ff60e3689def6a
Hanks <zhanghan.me@gmail.com>
2017-07-31 12:02:09 +0800

    add richtext examples

---
+++ src/components/richtext/complex.vue
@@ -0,0 +1,91 @@
+<template>
+  <scroller>
+    <div class="panel">
+      <richtext class="richtext">
+        <span class="grey">图片也能加链接</span>
+        <a href="https://m.taobao.com">
+          <image style="width:172px;height:67px" src="http://img.alicdn.com/tps/i2/TB1bNE7LFXXXXaOXFXXwFSA1XXX-292-116.png" />
+        </a>
+        <a href="https://m.tmall.com">
+          <image style="width:190px;height:100px" src="https://img.alicdn.com/tfs/TB1MaLKRXXXXXaWXFXXXXXXXXXX-480-260.png" />
+        </a>
+        <span style="font-size:36px">
+          <span>。文字内容也可以是动态更新的，</span>
+          <span style="font-weight:600">输入框里的字：</span>
+        </span>
+        <span class="type-text">{{userInput}}</span>
+      </richtext>
+    </div>
+    <div class="panel" style="flex-direction: row;">
+      <input type="text" class="input" v-model="userInput" />
+    </div>
+    <div class="panel" style="flex-direction: row;">
+      <text class="button" @click="back">返回</text>
+    </div>
+  </scroller>
+</template>
+
+<script>
+  const navigator = weex.requireModule('navigator')
+  const modal = weex.requireModule('modal')
+  export default {
+    data () {
+      return {
+        userInput: ''
+      }
+    },
+    methods: {
+      back () {
+        navigator.pop({
+          animated: "true"
+        }, event => {
+          modal.toast({ message: '从前一个页面返回了' })
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .panel {
+    width: 650px;
+    margin-top: 100px;
+    margin-left: 50px;
+    justify-content: center;
+  }
+  .richtext {
+    border-width: 2px;
+    border-style: solid;
+    border-color: #DDDDDD;
+    font-size: 26px;
+  }
+  .type-text {
+    color: #41B883;
+    font-size: 50px;
+  }
+  .input {
+    font-size: 40px;
+    width: 650px;
+    margin-top: 50px;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    padding-left: 20px;
+    padding-right: 20px;
+    color: #666666;
+    border-width: 1px;
+    border-style: solid;
+    border-color: #41B883;
+  }
+  .button {
+    width: 280px;
+    font-size: 66px;
+    padding-top: 15px;
+    padding-bottom: 15px;
+    text-align: center;
+    color: #666666;
+    border-width: 2px;
+    border-style: solid;
+    border-color: #DDDDDD;
+    background-color: #F5F5F5;
+  }
+</style>
---
+++ src/components/richtext/example.vue
@@ -0,0 +1,94 @@
+<template>
+  <scroller>
+    <div class="panel">
+      <richtext class="richtext">
+        <span class="label">图片：</span>
+        <image style="width:222px;height:81px" src="http://img.alicdn.com/tps/TB1wXN_PpXXXXbgapXXXXXXXXXX-222-81.png" />
+        <span style="font-size:35px">
+          <span class="grey">这是一段很长的文字，虽然很长但是并没什么卵用，只是用来测试能不能换行。</span>
+          <span>
+            <span>如果这些字会</span>
+            <span class="text-bold">变大变粗</span>
+            <span class="text-thin">变小变细</span>
+            <span style="color:#FF6600">还能变颜色</span>
+            <span>，就说明富文本组件能用。</span>
+          </span>
+        </span>
+      </richtext>
+    </div>
+    <div class="panel">
+      <richtext class="richtext" style="font-size:35px">
+        <span>富文本里的内容可以是动态获取的，</span>
+        <span style="color:#41B883">
+          <span>例如 Vue.js 的 start 数是：</span>
+          <span class="star-count">{{starCount}}</span>
+          <span>。</span>
+        </span>
+        <span>而且还可以添加链接：</span>
+        <a class="link" href="http://dotwe.org/raw/dist/8e3c274f5cae4a391a14ac81c890325f.bundle.wx">
+          <span>点击这行字能看到另一个例子</span>
+        </a>
+        <span>，点击其他字就跳转不了。</span>
+      </richtext>
+    </div>
+  </scroller>
+</template>
+
+<script>
+  const stream = weex.requireModule('stream')
+  export default {
+    data () {
+      return {
+        starCount: 'fetching...'
+      }
+    },
+    created () {
+      stream.fetch({
+        method: 'GET',
+        type: 'json',
+        url: 'https://api.github.com/repos/vuejs/vue'
+      }, ({ ok, data }) => {
+        this.starCount = ok ? data.stargazers_count : '(network error)'
+      })
+    }
+  }
+</script>
+
+<style scoped>
+  .panel {
+    width: 650px;
+    margin-top: 200px;
+    margin-left: 50px;
+    justify-content: center;
+  }
+  .richtext {
+    border-width: 2px;
+    border-style: solid;
+    border-color: #DDDDDD;
+  }
+  .grey {
+    color: #888888;
+    font-size: 24px;
+  }
+  .label {
+    color: blue;
+    font-size: 48px;
+  }
+  .text-bold {
+    font-size: 50px;
+    font-weight: 700;
+  }
+  .text-thin {
+    font-size: 18px;
+    font-weight: 100;
+  }
+  .link {
+    color: rgb(0, 180, 255);
+    font-size: 40px;
+    text-decoration: underline;
+  }
+  .star-count {
+    font-size: 48px;
+    font-weight: 700;
+  }
+</style>
