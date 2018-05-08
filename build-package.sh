4d476c86a500a244e1ec949197e92e899c6b0609
Hanks <zhanghan.me@gmail.com>
2018-05-08 13:29:41 +0800

    copy generated files to app package

---
+++ build-package.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function copy_page () {
+  cp dist/$1.weex.js package/pages/$1.weex.js
+}
+
+rm -rf package
+mkdir package package/pages
+
+npm run build
+
+cp src/app.json package/app.json
+cp dist/worker.weex.js package/app.js
+cp dist/shared.weex.js package/shared.js
+
+copy_page "about"
+copy_page "examples"
+copy_page "guide"
+copy_page "landing"
+copy_page "news"
