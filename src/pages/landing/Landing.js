da7fbced49807b3b93aca8f0b9432ea6f855194e
Hanks <zhanghan.me@gmail.com>
2018-05-08 13:54:45 +0800

    move app.js to landing page

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,6 +1,19 @@
 import '../../shared'
-import '../../app'
+import * as utils from '../../utils'
 import LandingPage from './Landing.vue'
+
+// hide title bar
+try {
+  const titleBar = weex.requireModule('titleBar')
+  titleBar.showTitleBar(false)
+} catch (e) {}
+
+// fetch and save examples
+setTimeout(() => {
+  utils.fetchExamples(utils.saveExamples)
+  utils.fetchGuide(utils.saveGuide)
+  utils.fetchAbout(utils.saveAbout)
+}, 10)
 
 LandingPage.el = '#root'
 new Vue(LandingPage)
