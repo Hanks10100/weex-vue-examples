0e5a11def3aff484fd740a7ef859086134900b0d
Hanks <zhanghan.me@gmail.com>
2018-05-18 14:43:45 +0800

    support to fetch remote data

--- src/app.js
+++ src/app.js
@@ -1,17 +1,44 @@
+import { fetchExamples, fetchGuide, fetchAbout, fetchNews } from './utils/index'
+import { guideLessons, aboutApp } from './utils/mock'
+const modal = requireModule('modal')
+
 App({
   globalData: {
     language: 'en',
+    systemLanguage: 'en',
+    followSystemLanguage: true,
     dict: {
-      LANGUAGE: { en: 'Language', zh: '语言' }
+      LANGUAGE: { en: 'Language', zh: '语言' },
+      LANGUAGE_TYPE: { en: 'English', zh: '简体中文' },
+      FOLLOW_SYSTEM: { en: 'Follow System', zh: '跟随系统' },
+      READ_MORE: { en: 'read more', zh: '查看更多' },
+      REFRESH: { en: 'Release to refresh', zh: '释放刷新' },
+      REFRESHING: { en: 'Fetching ...', zh: '正在加载……' },
+      UPDATED: { en: 'Updated', zh: '已更新' },
+      LOAD_MERE: { en: 'Load more', zh: '加载更多' },
+      NO_MORE_NEWS: { en: 'No more news', zh: '到底了' }
     },
-    examples: ['A', 'B', 'C'],
-    aboutApp: 'app'
+    examples: [],
+    guideLessons,
+    aboutApp,
+    news: []
   },
   onLaunch (...args) {
     console.log(` => app launch (${JSON.stringify(args)})`)
+    modal.toast({ message: 'App Launched!' })
+
+    fetchExamples(result => {
+      console.log(` => fetch examples: (${JSON.stringify(result)})`)
+      this.examples = result
+    }, error => {
+      console.log(` => failed to fetch examples: (${JSON.stringify(error)})`)
+    })
+    fetchAbout(result => this.aboutApp = result)
+    fetchGuide(result => this.guideLessons = result)
+    fetchNews(result => this.news = result)
   },
-  onShow (...args) {
-    console.log(` => app show (${JSON.stringify(args)})`)
+  onShow () {
+    console.log(` => app show`)
   },
   onHide () {
     console.log(' => app hide')
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,81 +1,11 @@
-const requireModule = ((context) => {
-  if (context && typeof context.requireModule === 'function') {
-    return context.requireModule
-  }
-  if (typeof weex === 'object' && weex
-    && typeof weex.requireModule === 'function') {
-    return weex.requireModule
-  }
-  return () => {}
-})(this)
+const _requireModule = typeof requireModule === 'function'
+  ? requireModule
+  : typeof weex.requireModule === 'function'
+    ? weex.requireModule
+    : (name) => console.log(`Can't require "${name}" module.`)
 
-const stream = requireModule('stream')
-const storage = requireModule('storage')
-const navigator = requireModule('navigator')
-
-const encoder = typeof encodeURIComponent === 'function'
-  ? encodeURIComponent
-  : typeof encodeURI === 'function'
-    ? encodeURI
-    : x => x
-
-function encodeParams (params) {
-  if (!params || typeof params !== 'object') {
-    return ''
-  }
-  const array = []
-  for (const key in params) {
-    if (typeof params[key] === 'string') {
-      array.push(`${encoder(key)}=${encoder(params[key])}`)
-    }
-  }
-  return array.join('&')
-}
-
-export function createLink (name, params = {}) {
-  const args = []
-  for (const key in params) {
-    if (typeof params[key] === 'string') {
-      args.push(`${encoder(key)}=${encoder(params[key])}`)
-    }
-  }
-  if (WXEnvironment.platform === 'Web') {
-    args.unshift(`page=${name}.web.js`)
-    return `/?${args.join('&')}`
-  }
-  const base = getBaseURL()
-  return `${base}${name}.weex.js` + (args.length ? `?${args.join('&')}` : '')
-}
-
-export function createURL (hash, params) {
-  if (WXEnvironment.platform === 'Web') {
-    return `http://dotwe.org/raw/htmlVue/${hash}`
-  }
-  const url = `http://dotwe.org/raw/dist/${hash}.bundle.wx`
-  const paramString = encodeParams(params)
-  if (WXEnvironment.appName === 'TB') {
-    return `${url}?_wx_tpl=${url}&${paramString}`
-  }
-  if (WXEnvironment.appName === 'WXSample') {
-    return `${url}?${paramString}`
-  }
-  return `${url}?wx_weex=true&${paramString}`
-}
-
-function getBaseURL () {
-  var bundleUrl = weex.config.bundleUrl
-  var isAndroidAssets = bundleUrl.indexOf('your_current_IP') >= 0 || bundleUrl.indexOf('file://assets/')>=0;
-  var isiOSAssets = bundleUrl.indexOf('file:///') >= 0 && bundleUrl.indexOf('WeexDemo.app') > 0;
-  if (isAndroidAssets) {
-    return 'file://assets/';
-  }
-  else if (isiOSAssets) {
-    // file:///var/mobile/Containers/Bundle/Application/{id}/WeexDemo.app/
-    // file:///Users/{user}/Library/Developer/CoreSimulator/Devices/{id}/data/Containers/Bundle/Application/{id}/WeexDemo.app/
-    return bundleUrl.substring(0, bundleUrl.lastIndexOf('/') + 1);
-  }
-  return ''
-}
+const network = _requireModule('network')
+const navigator = _requireModule('navigator')
 
 export function i18n (text, language) {
   if (typeof text === 'string') {
@@ -96,85 +26,23 @@ export function parseLanguage (language) {
   return ''
 }
 
-export const languageKey = 'WEEX_PLAYGROUND_LANGUAGE'
-
-export function getSystemLanguage (done, fail = () => {}) {
-  if (!typeof done === 'function') {
-    return
-  }
-  if (WXEnvironment.platform.toLowerCase() === 'web') {
-    const lang = parseLanguage(window.navigator.language)
-    lang ? done(lang) : fail()
-  } else {
-    try {
-      const locale = requireModule('locale') || requireModule('local')
-      let useSync = false
-      const resSync = locale.getLanguage(language => {
-        const lang = parseLanguage(language)
-        if (lang) {
-          useSync || done(lang)
-        } else {
-          fail()
-        }
-      })
-      const langSync = parseLanguage(resSync)
-      if (langSync) {
-        useSync = true
-        done(langSync)
-      } else {
-        fail()
-      }
-    } catch (e) {
-      fail(e)
-    }
-  }
-}
-
-export function jumpTo (url, title, lang) {
-  const language = lang || (this && this.language) || 'en'
-  storage.setItem('CURRENT_DOCUMENT_URL', i18n(url, language))
-  navigator.push({
-    url: createURL(
-      'bf0305c14b511b24a4e616f53926432b',
-      { language, title: i18n(title, language) }
-    )
-  })
-}
-
-export function viewSource (hash, lang) {
-  const language = lang || (this && this.language) || 'en'
-  navigator.push({
-    url: createURL(
-      'f6ce29faf686eabc38b410bf4828fa5a',
-      { hash, language }
-    )
-  })
-}
-
-const storageKeys = {
-  doodle: 'WEEX_PLAYGROUND_APP_DOODLE',
-  guide: 'WEEX_PLAYGROUND_APP_GUIDE',
-  examples: 'WEEX_PLAYGROUND_APP_EXAMPLES',
-  news: 'WEEX_PLAYGROUND_APP_NEWS',
-  about: 'WEEX_PLAYGROUND_APP_ABOUT',
-}
 export function fetchData (name, done = () => {}, fail = () => {}) {
   try {
-    stream.fetch({
+    network.request({
       url: 'http://dotwe.org/query/weex-playground-app',
       method: 'post',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
       },
-      type: 'json',
+      dataType: 'json',
       body: `name=${name}`
     }, res => {
-      if (res.ok && res.data && res.data.success) {
-        done(res.data)
+      if (res.ok && res.data && res.data[name]) {
+        done(res.data[name])
       } else {
         fail(res)
       }
-    })
+    }, error => fail(error))
   } catch (err) {
     fail(err)
   }
@@ -183,5 +51,4 @@ export function fetchData (name, done = () => {}, fail = () => {}) {
 export const fetchExamples = (...args) => fetchData('examples', ...args)
 export const fetchGuide = (...args) => fetchData('guide', ...args)
 export const fetchAbout = (...args) => fetchData('about', ...args)
-export const fetchDoodle = (...args) => fetchData('doodle', ...args)
 export const fetchNews = (...args) => fetchData('news', ...args)
