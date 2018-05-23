8b279ee374e983ec6513b253c461b31caf072dc7
Hanks <zhanghan.me@gmail.com>
2018-05-23 21:47:36 +0800

    modify build-package.sh script

--- build-package.sh
+++ build-package.sh
@@ -2,24 +2,18 @@
 
 # for mini-app package
 function copy_page () {
-  cp dist/$1.weex.js package/pages/$1.weex.js
+  cp dist/$1.weex.js build/pages/$1.weex.js
+  cp dist/$1.weex.js build/tbapp/pages/$1.weex.js
 }
 
-# for multi-platform bundle
-# function copy_page () {
-#   mkdir package/pages/$1
-#   cp dist/$1.weex.js package/pages/$1/index.weex.js
-#   cp dist/$1.web.js package/pages/$1/index.web.js
-# }
-
-rm -rf package
-mkdir package package/pages
+rm -rf build
+mkdir build build/tbapp build/pages build/tbapp/pages
 
 npm run build
 
-cp ./app.config.json package/app.config.json
-cp dist/app.weex.js package/app.js
-cp dist/share.weex.js package/share.js
+cp ./app.config.raw.json build/tbapp/app.config.json
+cp dist/app.weex.js build/tbapp/app.js
+cp dist/share.weex.js build/tbapp/share.js
 
 copy_page "about"
 copy_page "examples"
