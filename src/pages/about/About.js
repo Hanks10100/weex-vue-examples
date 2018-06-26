b4a724bbe1e364f6cb3a14b51af69c069bfe4a36
erha19 <faterrole@gmail.com>
2018-06-26 15:03:20 +0800

    Make child page always has a title bar

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,5 +1,11 @@
 import '../../shared'
 import About from './About.vue'
 
+// show title bar
+try {
+  const titleBar = weex.requireModule('titleBar')
+  titleBar.showTitleBar(true)
+} catch (e) {}
+
 About.el = '#root'
 new Vue(About)
