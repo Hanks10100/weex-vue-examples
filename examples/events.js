1c13232b2b85d9720ff40e53c482e7b389041b4b
codefurture <gubaojian@163.com>
2018-12-13 11:40:34 +0800

    remove touch move left demo
    remove touch move left demo

--- examples/events.js
+++ examples/events.js
@@ -5,10 +5,6 @@ module.exports = {
     type: 'touch',
     name: { zh: 'Touch事件', en: 'Touch Event' },
     examples: [{
-      hash: '993eb7f1a379a224c2ec5224370b2b76',
-      title: '<touch left move>',
-      screenshot: 'https://gw.alicdn.com/tfs/TB1D7DhAWmWBuNjy1XaXXXCbXXa-540-844.png'
-    }, {
       hash: '06c5fd1948ba95d09ece274902ccf425',
       title: '<touch ball>',
       screenshot: 'https://gw.alicdn.com/tfs/TB1UpULAMmTBuNjy1XbXXaMrVXa-540-844.png'
